f3dc12dae6f2e27e774d692546bd66f459654a86
Wenyn520 <68536826+Wenyn520@users.noreply.github.com>
2021-01-29 17:40:28 +0800

    Case
    composition

---
+++ Case.java
@@ -0,0 +1,31 @@
+//composition
+public class Case {
+    private String model, manufacturer, powerSupply;
+    private Dimensions dimensions;
+
+    public Case(String model, String manufacturer, String powerSupply, Dimensions dimensions) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.powerSupply = powerSupply;
+        this.dimensions = dimensions;
+    }
+    public void pressPowerButton(){
+        System.out.println("Pressed power button");
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getPowerSupply() {
+        return powerSupply;
+    }
+
+    public Dimensions getDimensions() {
+        return dimensions;
+    }
+}
