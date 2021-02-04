97d3e92d0e574170848f3c19da8eee2d41fc24d2
Wenyn520 <68536826+Wenyn520@users.noreply.github.com>
2021-02-04 15:54:05 +0800

    ComplexNumber
    OOP - Complex operations

---
+++ ComplexNumber.java
@@ -0,0 +1,31 @@
+//coding exercise 34 - Complex operations
+public class ComplexNumber {
+    private double real, imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return this.real;
+    }
+
+    public double getImaginary() {
+        return this.imaginary;
+    }
+    public void add (ComplexNumber complexNumber){
+        add(complexNumber.real, complexNumber.imaginary);
+    }
+    public void add (double real, double imaginary){
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+    public void subtract (ComplexNumber complexNumber){
+        subtract(complexNumber.real, complexNumber.imaginary);
+    }
+    public void subtract (double real, double imaginary){
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+}
