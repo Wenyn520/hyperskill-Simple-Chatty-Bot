e3932926a08b069a95c2604f91668f77c5b4a329
Wenyn520 <68536826+Wenyn520@users.noreply.github.com>
2021-02-04 15:53:10 +0800

    Circle
    OOP - Cylinder

---
+++ Circle.java
@@ -0,0 +1,17 @@
+//coding exercise 35 - Cylinder
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+        if (radius<0){
+            this.radius = 0;
+        }
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public double getArea (){
+        return Math.PI * radius*radius;
+    }
+}
