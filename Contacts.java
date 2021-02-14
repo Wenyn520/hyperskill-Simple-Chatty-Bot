822e298ef455a5890b24afd50379ad75bc2ce07a
Wenyn520 <68536826+Wenyn520@users.noreply.github.com>
2021-02-14 20:01:07 +0800

    Contacts
    ArrayList challenge

---
+++ Contacts.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+//coding challenge - ArrayList challenge
+//my solution
+public class Contacts {
+
+    private String name;
+    private String mobileNo;
+
+    public Contacts(String name, String mobileNo) {
+        this.name = name;
+        this.mobileNo = mobileNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+}
